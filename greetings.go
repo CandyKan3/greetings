5bf303048e6d1af875cebdda2dcb51c877c68e97
Bryce <brycekane5@gmail.com>
2021-04-14 12:22:13 -0400

    added randomization

--- greetings.go
+++ greetings.go
@@ -3,6 +3,8 @@ package greetings
 import (
 	"errors"
 	"fmt"
+	"math/rand"
+	"time"
 )
 
 // Hello returns a greeting for the named person.
@@ -13,6 +15,17 @@ func Hello(name string) (string, error) {
 	}
 
 	// If a name was received, return a value that embeds the name
-	message := fmt.Sprintf("Hi, %v. Welcome!", name)
+	message := fmt.Sprintf(randomFormat(), name)
 	return message, nil
+}
+func init() {
+	rand.Seed(time.Now().UnixNano())
+}
+func randomFormat() string {
+	formats := []string{
+		"Hi, %v. Welcome!",
+		"Great to see you, %v",
+		"Hail, %v! well met!",
+	}
+	return formats[rand.Intn(len(formats))]
 }
