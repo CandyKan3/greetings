8d52d6d340b267843cf92827def5755551923c0a
Bryce <brycekane5@gmail.com>
2021-04-14 12:46:26 -0400

    added multiple name greetings

--- greetings.go
+++ greetings.go
@@ -18,6 +18,18 @@ func Hello(name string) (string, error) {
 	message := fmt.Sprintf(randomFormat(), name)
 	return message, nil
 }
+func Hellos(names []string) (map[string]string, error) {
+	//A map to associate names with messages
+	messages := make(map[string]string)
+	for _, name := range names {
+		message, err := Hello(name)
+		if err != nil {
+			return nil, err
+		}
+		messages[name] = message
+	}
+	return messages, nil
+}
 func init() {
 	rand.Seed(time.Now().UnixNano())
 }
